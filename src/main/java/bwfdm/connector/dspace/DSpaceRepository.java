4d106bf7514897a6c3d9cd62fa142e42c9c963de
Volodymyr Kushnarenko <volodymyr.kushnarenko@uni-ulm.de>
2018-08-01 16:28:35 +0200

    Format DSpaceRepository

--- src/main/java/bwfdm/connector/dspace/DSpaceRepository.java
+++ src/main/java/bwfdm/connector/dspace/DSpaceRepository.java
@@ -33,33 +33,38 @@ import org.swordapp.client.ServiceDocument;
 import org.swordapp.client.UriRegistry;
 
 /**
- * The class consists implementation of some general purpose DSpace-specific methods.
- * It should be used as a parent for further child-classes as e.g. DSpace_v6, Dspace_v5.
+ * The class consists implementation of some general purpose DSpace-specific
+ * methods. It should be used as a parent for further child-classes as e.g.
+ * DSpace_v6, Dspace_v5.
  * 
  * @author Volodymyr Kushnarenko
  *
  */
-public abstract class DSpaceRepository implements PublicationRepository{
+public abstract class DSpaceRepository implements PublicationRepository {
 
 	private static final Logger log = LoggerFactory.getLogger(DSpaceRepository.class);
-	
+
 	// Header constants
 	public static final String APPLICATION_JSON = "application/json";
 	public static final String CONTENT_TYPE_HEADER = "Content-Type";
 	public static final String ACCEPT_HEADER = "Accept";
-	
-	
+
 	/*
-	 * -------------------------------
+	 * ----------------------- 
+	 * 
 	 * General purpose methods
-	 * -------------------------------
+	 * 
+	 * -----------------------
 	 */
-	
-	
+
 	/**
-	 * Get new authentication credentials. 
-	 * <p> To disactivate "on-behalf-of" option please use the same string for "adminUser" and "userLogin".
-	 * <p> If "adminUser" and "userLogin" are different, "on-behalf-of" option will be used.
+	 * Get new authentication credentials.
+	 * <p>
+	 * To disactivate "on-behalf-of" option please use the same string for
+	 * "adminUser" and "userLogin".
+	 * <p>
+	 * If "adminUser" and "userLogin" are different, "on-behalf-of" option will be
+	 * used.
 	 * 
 	 * @param adminUser
 	 * @param adminPassword
@@ -67,15 +72,14 @@ public abstract class DSpaceRepository implements PublicationRepository{
 	 * @return
 	 */
 	public static AuthCredentials getNewAuthCredentials(String adminUser, char[] adminPassword, String userLogin) {
-		
-		if(adminUser.equals(userLogin)) {
+
+		if (adminUser.equals(userLogin)) {
 			return new AuthCredentials(userLogin, String.valueOf(adminPassword)); // without "on-behalf-of"
 		} else {
 			return new AuthCredentials(adminUser, String.valueOf(adminPassword), userLogin); // with "on-behalf-of"
 		}
 	}
-	
-	
+
 	/**
 	 * Get service document via SWORD v2
 	 * 
@@ -84,28 +88,28 @@ public abstract class DSpaceRepository implements PublicationRepository{
 	 * @param authCredentials
 	 * @return ServiceDocument or null in case of error/exception
 	 */
-	public static ServiceDocument getServiceDocument(SWORDClient swordClient, String serviceDocumentURL, AuthCredentials authCredentials) {
+	public static ServiceDocument getServiceDocument(SWORDClient swordClient, String serviceDocumentURL,
+			AuthCredentials authCredentials) {
 		ServiceDocument serviceDocument = null;
 		try {
 			serviceDocument = swordClient.getServiceDocument(serviceDocumentURL, authCredentials);
 		} catch (SWORDClientException | ProtocolViolationException e) {
-			log.error("Exception by accessing service document: " + e.getClass().getSimpleName() + ": " + e.getMessage());
+			log.error("Exception by accessing service document: {}: {}", e.getClass().getSimpleName(), e.getMessage());
 			return null;
 		}
 		return serviceDocument;
 	}
-	
-	
+
 	/**
 	 * Get available collections via SWORD v2
 	 * 
 	 * @return Map<String, String> where key=URL, value=Title
 	 */
-	public static Map<String, String> getAvailableCollectionsViaSWORD(ServiceDocument serviceDocument){
+	public static Map<String, String> getAvailableCollectionsViaSWORD(ServiceDocument serviceDocument) {
 		Map<String, String> collections = new HashMap<String, String>();
-		
-		if(serviceDocument != null) {
-			for(SWORDWorkspace workspace : serviceDocument.getWorkspaces()) {
+
+		if (serviceDocument != null) {
+			for (SWORDWorkspace workspace : serviceDocument.getWorkspaces()) {
 				for (SWORDCollection collection : workspace.getCollections()) {
 					// key = full URL, value = Title
 					collections.put(collection.getHref().toString(), collection.getTitle());
@@ -114,66 +118,67 @@ public abstract class DSpaceRepository implements PublicationRepository{
 		}
 		return collections;
 	}
-	
-	
+
 	/**
-	 * Get a file extension (without a dot) from the file name (e.g. "txt", "zip", ...)
+	 * Get a file extension (without a dot) from the file name 
+	 * (e.g. "txt", "zip", * ...)
+	 * 
 	 * @param fileName
 	 * @return
 	 */
-	public static String getFileExtension(String fileName) {	
+	public static String getFileExtension(String fileName) {
 		String extension = "";
 		int i = fileName.lastIndexOf('.');
-		if(i>0) {
-			extension = fileName.substring(i+1);
+		if (i > 0) {
+			extension = fileName.substring(i + 1);
 		}
-		return extension;		
+		return extension;
 	}
-	
-	
+
 	/**
-	 * Get package format basing on the file name.
-	 * E.g. {@link UriRegistry.PACKAGE_SIMPLE_ZIP} {@link UriRegistry.PACKAGE_BINARY}
+	 * Get package format basing on the file name. 
+	 * E.g. {@link UriRegistry.PACKAGE_SIMPLE_ZIP} or {@link UriRegistry.PACKAGE_BINARY}
+	 * 
 	 * @param fileName
-	 * @return 
+	 * @return
 	 */
 	public static String getPackageFormat(String fileName) {
 		String extension = getFileExtension(fileName);
-		
-		if(extension.toLowerCase().equals("zip")) {
+
+		if (extension.toLowerCase().equals("zip")) {
 			return UriRegistry.PACKAGE_SIMPLE_ZIP;
 		}
 		return UriRegistry.PACKAGE_BINARY;
 	}
-		
-	
+
 	/*
-	 * -------------
-	 * Extra classes
+	 * ------------- 
+	 * 
+	 * Extra classes 
+	 * 
 	 * -------------
 	 */
-	
-	
-	public static enum SwordRequestType {	
-		DEPOSIT("DEPOSIT"), //"POST" request
-		REPLACE("REPLACE"), //"PUT" request
-		DELETE("DELETE")	//reserved for the future
+
+	public static enum SwordRequestType {
+		DEPOSIT("DEPOSIT"), // "POST" request
+		REPLACE("REPLACE"), // "PUT" request
+		DELETE("DELETE") // reserved for the future
 		;
-		
+
 		private final String label;
-		
+
 		private SwordRequestType(String label) {
 			this.label = label;
 		}
-		
+
 		public String getLabel() {
 			return label;
 		}
-		
+
 		@Override
 		public String toString() {
 			return label;
 		}
 	}
-		
+
 }
